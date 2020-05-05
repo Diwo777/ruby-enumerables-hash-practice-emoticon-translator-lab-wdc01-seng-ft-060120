93725eea249ebc57f13070bd12a9ac5de1c0f9c9
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-05 18:22:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,25 @@
-# require modules here
+require 'yaml'
+require 'pry' 
+def load_library(yaml_file)
+   output = Hash.new
+   library = YAML.load_file(yaml_file)
+   library.each do |key, value|
+    output[key] = {}
+    output[key][:english] = value[0]
+    output[key][:japanese] = value[1]
+  end
+output
+end	
 
-def load_library
-  # code goes here
+def get_english_meaning(yaml_file,j_emoticon)
+ library = load_library(yaml_file)
+ library.each do |key,value|
+ binding.pry
+end
 end
 
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
-def get_english_meaning
-  # code goes here
-end
+
