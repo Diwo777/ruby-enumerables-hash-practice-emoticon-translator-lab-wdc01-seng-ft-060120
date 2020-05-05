ac39ef3f617932852da94712468cca668b8d5fd3
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-05 18:39:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ def get_english_meaning(yaml_file,j_emoticon)
  library.each do |key,value|
    if j_emoticon == value[:japanese]
      return  key
-   else if
+   else 
      return "Sorry, that emoticon was not found"
    
    
