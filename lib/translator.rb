9bbe26449e46e1922049cf0a0a98cd6a9d410690
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-05 18:24:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(yaml_file)
    output = Hash.new
    library = YAML.load_file(yaml_file)
    library.each do |key, value|
-      binding.pry
+      
     output[key] = {}
     output[key][:english] = value[0]
     output[key][:japanese] = value[1]
@@ -15,7 +15,7 @@ end
 def get_english_meaning(yaml_file,j_emoticon)
  library = YAML.load_file(yaml_file)
  library.each do |key,value|
-
+binding.pry
 end
 end
 
